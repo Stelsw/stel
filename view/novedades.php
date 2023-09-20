836651d2fe1d40272b0e915f43a83f4abf383cce
David-olaya <124094645+David-olaya@users.noreply.github.com>
2023-09-19 21:34:21 -0500

    Novedades
    Cambios de estilo, en las tablas

--- view/novedades.php
+++ view/novedades.php
@@ -4,7 +4,8 @@ if (!isset($_SESSION['usuario'])) {
     //si no existe usuario
     header('Location: ../view/AccesoDenegado.php');
 }else { ?>
-    <!doctype html>
+
+<!doctype html>
     <html lang="en">
     
         <head>
@@ -12,12 +13,22 @@ if (!isset($_SESSION['usuario'])) {
             <meta charset="utf-8">
             <meta name="viewport" content="width=device-width, initial-scale=1">
             <title>STEL</title>
-            
+
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
             <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
             <link rel="shortcout icon" href="0. Imagenes/0. Logos/3. logo sin  nombre blanco.png">
             <link rel="stylesheet" type="text/css" href="1. CSS/2. Modulos.css">
 
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0/css/bootstrap.min.css">
+            <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap5.min.css">
+            <link rel="stylesheet" type="text/css" href="1. CSS/2. Modulos.css">
+
+            <script src="https://code.jquery.com/jquery-3.5.1.js" defer></script>
+            <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js" defer></script>
+            <script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js" defer></script>
+            
+        
+        
         </head>
         <body>
         
@@ -102,7 +113,7 @@ if (!isset($_SESSION['usuario'])) {
                             </li>
 
                             <li class="nav-item align-self-center">
-                                <a class="nav-link active text-white d-flex justify-content-end" aria-current="page" href="index.html">Salir</a>
+                                <a class="nav-link active text-white d-flex justify-content-end" aria-current="page" href="../controller/logout.php">Cerrar sesión</a>
                             </li>
                         
                         </ul>
@@ -115,6 +126,57 @@ if (!isset($_SESSION['usuario'])) {
             </nav>
 
             <!--  -->
+        
+        <!--<div class="container d-text-center pt-4">-->
+
+        <h2 class="text-center p-3">  Novedades</h2>      
+        <div class="row justify-content-center">
+
+    <div class="col-4 p-3 col-md-9 col-sm-5">
+        <table class="table table-hover">
+            <thead class="table-light">
+                <tr>
+                <th scope="col">pkidNovedades</th>
+                <th scope="col">Remitente</th>
+                <th scope="col">tipoNovedad</th>
+                <th scope="col">Asunto</th>
+                <th scope="col">Descripcion</th>
+                <th scope="col">Documento</th>
+                <th scope="col">Fecha</th>
+                <th scope="col">Respuesta</th>
+                <th scope="col">Estado</th>
+                <th scope="col"></th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                include "../model/conexion.php";
+                $sql=$conn->query("SELECT * FROM tbl_novedades");
+                while ($datos=$sql->fetch_object()){ ?>
+                    <div class="col-4 m-5 col-md-9 col-sm-5">
+                        <tr>
+                            <td><?= $datos->pkidNovedades?></td>
+                            <td><?= $datos->remNovedades?></td>
+                            <td><?= $datos->TipoNovedad?></td>
+                            <td><?= $datos->asuntoNovedades?></td>
+                            <td><?= $datos->descNovedades?></td>
+                            <td><?= $datos->docNovedades?></td>
+                            <td><?= $datos->fecNovedades?></td>
+                            <td><?= $datos->resNovedades?></td>
+                            <td><?= $datos->estNovedades?></td>    
+                            <td>
+                              
+                            </td>
+                        </tr>
+                    </div>    
+                <?php }
+                ?>
+            </tbody>
+        </table>
+    </div>
+
+  
+    </body>
 
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 
